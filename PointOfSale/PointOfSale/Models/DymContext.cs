6dc8356470521035bf0108be041608602e89d74c
Jesús Mendoza <mendoza.git@gmail.com>
2019-08-17 14:10:40 -0500

    Corrige la cadena de conexion con la db

--- PointOfSale/PointOfSale/Models/DymContext.cs
+++ PointOfSale/PointOfSale/Models/DymContext.cs
@@ -84,8 +84,9 @@ namespace PointOfSale.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=Dym;Trusted_Connection=True;");
+
+                //optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=Dym;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(@"Server=192.168.1.5;Database=Dym;User Id=SA;Password = 12345678;");
             }
         }
 
