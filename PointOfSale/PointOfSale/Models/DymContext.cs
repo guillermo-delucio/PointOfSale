f6662374e49e450892da5f5e2ea8ddda4a4375c5
JesusMendoza <mendoza.git@gmail.com>
2019-10-02 17:08:59 -0500

    Corrige la importacion de lotes

--- PointOfSale/PointOfSale/Models/DymContext.cs
+++ PointOfSale/PointOfSale/Models/DymContext.cs
@@ -86,7 +86,8 @@ namespace PointOfSale.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=Dym;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server = 192.168.1.5; Database = Dym; User Id = sa;Password = 12345678;");
+                
             }
         }
 
